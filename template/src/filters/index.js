220af5cd5f47083bed087340fa48a1f856875235
zhdsh <39177191+zhdsh@users.noreply.github.com>
2018-05-17 15:10:06 +0800

    Update index.js

--- template/src/filters/index.js
+++ template/src/filters/index.js
@@ -1,23 +1,3 @@
-//过滤器
-//Vue.js 允许你自定义过滤器，可被用于一些常见的文本格式化。
-//过滤器可以用在两个地方：双花括号插值和 v-bind 表达式 (后者从 2.1.0+ 开始支持)。
-//过滤器应该被添加在 JavaScript 表达式的尾部，由“管道”符号指示：
-//<!-- 在双花括号中 -->
-//{{ message | capitalize }}
-//
-//<!-- 在 `v-bind` 中 -->
-//<div v-bind:id="rawId | formatId"></div>
-
-//过滤器可以串联：
-//{{ message | filterA | filterB }}
-
-//过滤器是 JavaScript 函数，因此可以接收参数：
-//{{ message | filterA('arg1', arg2) }}
-//这里，filterA 被定义为接收三个参数的过滤器函数。
-//其中 message 的值作为第一个参数，普通字符串 'arg1' 作为第二个参数，表达式 arg2 的值作为第三个参数。
-
-//可以在一个组件的选项中定义本地的过滤器：
-//也可以在创建 Vue 实例之前全局定义过滤器：
 
 const filters = {
 	//首字母大写
